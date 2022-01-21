640fd77b8a499e91c9e4a9c41ab753f692729016
Arjie <arjie.bongcas@splitsecondsoftware.com>
2022-01-21 23:03:18 +0800

    t2 max point

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -140,7 +140,7 @@ class ChartController extends Controller
                                         'prime' => $score['prime'],
                                         'segment' => ($segment + 1),
                                         'date' => $date,
-                                        'tcount' => $tcount,
+                                        'tcount' => $legend['name'] == 't2' ? $tcount * 100 : $tcount,
                                         'gscore' => $score['gscore'],
                                         'percentage' => $score['percentage'],
                                         'record_ids' => $records[$s_key]->pluck('id'),
