63cc1505bd1d952db6429856c061dbc8be2830cb
Cris <crisjohnreytarpin@gmail.com>
2022-01-27 17:34:28 +0800

    updates

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -16,30 +16,66 @@ class RecordController extends Controller
         $country_t5 = [
             'us' => '244',
             'sg' => '981',
-            'hk' => '982',
+            'hk' => '1092',
             'ca' => '983'
+        ];
+        $country_t3 = [
+            'us' => '242',
+            'sg' => '242',
+            'hk' => '1087',
+            'ca' => '242'
+        ];
+        $country_t4 = [
+            'us' => '243',
+            'sg' => '243',
+            'hk' => '1088',
+            'ca' => '243'
+        ];
+        $country_t8 = [
+            'us' => '245',
+            'sg' => '245',
+            'hk' => '1089',
+            'ca' => '245'
+        ];
+        $country_t9 = [
+            'us' => '246',
+            'sg' => '246',
+            'hk' => '1090',
+            'ca' => '246'
+        ];
+        $country_t10 = [
+            'us' => '247',
+            'sg' => '247',
+            'hk' => '1091',
+            'ca' => '247'
+        ];
+        $country_link = [
+            'us' => 'V3n',
+            'sg' => 'V3n',
+            'hk' => '5vw',
+            'ca' => 'V3n'
         ];
 
         $country = $request->country ?? 'us';
 
-        $http = Http::get($this->act_api . "survey_id=242&id={$request->id}");
+        $http = Http::get($this->act_api . "survey_id=" . $country_t3[$country] . "&id={$request->id}");
         $data = [
             't3' => json_decode($http->body(), true)
         ];
         
-        $http = Http::get($this->act_api . "survey_id=243&id={$request->id}");
+        $http = Http::get($this->act_api . "survey_id=" . $country_t4[$country] . "&id={$request->id}");
         $data['t4'] = json_decode($http->body(), true);
 
         $http = Http::get($this->act_api . "survey_id=" . $country_t5[$country] . "&id={$request->id}");
         $data['t5'] = json_decode($http->body(), true);
 
-        $http = Http::get($this->act_api . "survey_id=245&id={$request->id}");
+        $http = Http::get($this->act_api . "survey_id=" . $country_t8[$country] . "&id={$request->id}");
         $data['t8'] = json_decode($http->body(), true);
 
-        $http = Http::get($this->act_api . "survey_id=246&id={$request->id}");
+        $http = Http::get($this->act_api . "survey_id=" . $country_t9[$country] . "&id={$request->id}");
         $data['t9'] = json_decode($http->body(), true);
 
-        $http = Http::get($this->act_api . "survey_id=247&id={$request->id}");
+        $http = Http::get($this->act_api . "survey_id=" . $country_t10[$country] . "&id={$request->id}");
         $data['t10'] = json_decode($http->body(), true);
 
         $data['t6'] = $request->c1;
@@ -58,6 +94,6 @@ class RecordController extends Controller
             // 'created_at' => now()->addMonth(),
             // 'updated_at' => now()->addMonth()
         ]);
-        return redirect('https://fluent.splitsecondsurveys.co.uk/engine/complete/V3n?' . http_build_query($request->all()));
+        return redirect("https://fluent.splitsecondsurveys.co.uk/engine/complete/" . $country_link[$country] . "?" . http_build_query($request->all()));
     }
 }
