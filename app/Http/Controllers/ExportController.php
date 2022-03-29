24f40a2a2e75c28a52f5683cbab7dc2e671c3e9c
Arjie <arjie.bongcas@splitsecondsoftware.com>
2022-03-29 23:10:10 +0800

    bk5

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -565,7 +565,7 @@ class ExportController extends Controller
             ],
             "T46" => [ //K5
                 "Q_num" => '',
-                "USA" => ['k5', 'a26', 'c26'],
+                "USA" => ['k5', 'a27', 'c27'],
                 "Singapore" => ['y5', 'i14'],
                 "Hongkong" => ['k5'],
                 "Canada" => ['u5', 'i15']
