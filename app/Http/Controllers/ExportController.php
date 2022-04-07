9d63ceeed01e37a7682dfad8e7a252ebba6a8044
Arjie <arjie.bongcas@splitsecondsoftware.com>
2022-04-07 18:34:49 +0800

    dd

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -882,6 +882,7 @@ class ExportController extends Controller
         if ($count  == 0) {
             $equivalent = Chart::items($t, $prime);
             $tmp_result[3] = $equivalent;
+            dd($equivalent);
                 /* foreach ($tmp_data['data'] as $p_key => $tmp) {
                     $tmp_result[3] = $equivalent['equivalent'];//prime
                     $tmp_result[4+$p_key] = 0;//initialize
