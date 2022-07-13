a5e9ade0149eca4234bfdbc935875911cc86cc13
Arjie <arjie.bongcas@splitsecondsoftware.com>
2022-07-13 11:50:50 +0800

    exclude skipped

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -1086,6 +1086,7 @@ class ExportController extends Controller
                     case 't10':
                         $responses = collect($record->data[$t]['responses'][0]['primes'])->firstWhere('index', $prime);
                         if ($responses != null) {
+                            $val = '-';
                             foreach ($responses['data'] as $reskey => $resdata) {
                                 if ($resdata['selected']) {
                                     $val = $reskey + 1;
